8c43121300db2e38cbc8544aa9e8a4b111badc17
Rytoff <roman.rytoff@gmail.com>
2021-12-30 19:52:49 +0200

    L33 finished

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,16 +1,17 @@
-import {$} from '@core/dom'
+import { $ } from '@core/dom'
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
   getRoot() {
     const $root = $.create('div', 'excel')
 
-    this.components.forEach(Component => {
+    this.components.forEach((Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      // $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
     return $root
