93e2d18958860f0eff57c53ccf3f86292a654b1e
Rytoff <roman.rytoff@gmail.com>
2022-01-02 19:45:09 +0200

    L38 finished

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -9,20 +9,20 @@ function toColumn(col) {
   `
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
     <div class='row'>
-      <div class='row-info'></div>
+      <div class='row-info'>${index ? index : ''}</div>
       <div class='row-data'>${content}</div>
     </div>
   `
 }
 
-// function createCell() {
-//   return `
-//     <div class='cell' contenteditable>A1</div>
-//   `
-// }
+function toCell() {
+  return `
+    <div class='cell' contenteditable></div>
+  `
+}
 
 function toChar(_, idx) {
   return String.fromCharCode(CODES.A + idx)
@@ -31,16 +31,13 @@ function toChar(_, idx) {
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
-  const cols = new Array(colsCount)
-    .fill('')
-    .map(toChar)
-    .map(toColumn)
-    .join('')
+  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
+    const cells = new Array(colsCount).fill('').map(toCell).join('')
+    rows.push(createRow(i + 1, cells))
   }
 
   return rows.join('')
