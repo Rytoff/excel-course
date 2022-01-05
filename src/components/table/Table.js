04d4268e72ede6653516ea7367f2bd9f9e9a68fa
Rytoff <roman.rytoff@gmail.com>
2022-01-05 20:40:57 +0200

    L42 finished

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,9 +4,26 @@ import { createTable } from '@/components/table/table.template'
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor()
+  constructor($root) {
+    super($root, {
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    })
+  }
 
   toHTML() {
     return createTable()
   }
+
+  // onClick(event) {
+  //   console.log('click', event.target)
+  // }
+  // onMousedown() {
+  //   console.log('mousedown')
+  // }
+  // onMousemove() {
+  //   console.log('mousemove')
+  // }
+  // onMouseup() {
+  //   console.log('mouseup')
+  // }
 }
