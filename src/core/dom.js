4cf411ac86c100caf6778e96c3cb322b3dabf079
Rytoff <roman.rytoff@gmail.com>
2022-01-08 13:05:56 +0200

    L48 finished

--- src/core/dom.js
+++ src/core/dom.js
@@ -44,7 +44,7 @@ class Dom {
     return this.$el.querySelectorAll(selector)
   }
   css(styles = {}) {
-    
+    Object.keys(styles).forEach((key) => (this.$el.style[key] = styles[key]))
   }
 }
 
