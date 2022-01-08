2d00cb1221b8f341e39ab4c9e21873651ea34316
Rytoff <roman.rytoff@gmail.com>
2022-01-08 12:09:04 +0200

    L46 finished

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -23,14 +23,15 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target)
       const $parent = $resizer.closest('[data-type="resizible"]')
       const coords = $parent.getCoords()
-      console.log($parent.data)
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
 
       document.onmousemove = (e) => {
+        console.log('move')
         const delta = e.pageX - coords.right
         const value = coords.width + delta
         $parent.$el.style.width = value + 'px'
-        document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
-          .forEach(el => el.style.width = value + 'px')
+        cells.forEach((el) => (el.style.width = value + 'px'))
       }
       document.onmouseup = () => {
         document.onmousemove = null
@@ -44,3 +45,7 @@ export class Table extends ExcelComponent {
   //   console.log('mouseup')
   // }
 }
+// 334 ms  Scripting
+// 3069 ms  Rendering
+// 300 ms  Scripting
+// 2872 ms  Rendering
