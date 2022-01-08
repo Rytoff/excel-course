e491e101aaa7f74424d3b930f98a62447b869bbb
Rytoff <roman.rytoff@gmail.com>
2022-01-08 19:13:03 +0200

    L55 finished

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,9 @@ import { resizeHandler } from '@/components/table/table.resize'
 import { shouldResize } from '@/components/table/table.functions'
 import { TableSelection } from '@/components/table/TableSelection'
 import { isCell } from '@/components/table/table.functions'
+import { range } from '../../core/utils'
 import { $ } from '@core/dom'
+import { matrix } from './table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -35,7 +37,17 @@ export class Table extends ExcelComponent {
       resizeHandler(this.$root, event)
     } else if (isCell(event)) {
       const $target = $(event.target)
-      this.selection.select($target)
+      if (event.shiftKey) {
+        // const target = $target.id(true)
+        // const current = this.selection.current.id(true)
+
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`)
+        )
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
+      }
     }
   }
 }
