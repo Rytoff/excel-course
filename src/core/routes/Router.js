4a3798e5746067ff01a422c16822fb08ba4f4bd1
Rytoff <roman.rytoff@gmail.com>
2022-01-24 16:02:25 +0200

    L86 finished

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,5 @@
 import { $ } from '@core/dom'
-// import { ActiveRoute } from './ActiveRoute'
+import { ActiveRoute } from './ActiveRoute'
 
 export class Router {
   constructor(selector, routes) {
@@ -8,6 +8,8 @@ export class Router {
     }
     this.$placeholder = $(selector)
     this.routes = routes
+
+    this.page = null
 
     this.changePageHandler = this.changePageHandler.bind(this)
 
@@ -19,11 +21,19 @@ export class Router {
     this.changePageHandler()
   }
   changePageHandler() {
-    const Page = this.routes.excel
-    const page = new Page()
-    this.$placeholder.append(page.getRoot())
+    if (this.page) {
+      this.page.destroy()
+    }
+    this.$placeholder.clear()
 
-    page.afterRender()
+    const Page = ActiveRoute.path.includes('excel')
+      ? this.routes.excel
+      : this.routes.dashboard
+
+    this.page = new Page(ActiveRoute.param)
+    this.$placeholder.append(this.page.getRoot())
+
+    this.page.afterRender()
   }
   destroy() {
     window.removeEventListener('hashchange', this.changePageHandler)
